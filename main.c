271f9407fbb4933a71a21e658c22730a1c1a1011
José Victor <josevictorvasc@gmail.com>
2024-05-05 14:39:01 -0300

    Teste

--- main.c
+++ main.c
@@ -9,42 +9,34 @@
 #define NUM_SENSORS 4
 #define NUM_ACTUATORS 4
 
-
+pthread_t sensorsThread[NUM_SENSORS];
 int sensorialData[NUM_SENSORS];
-//Os atuadores são acessados via dicionário, como no C não existe essa estrutura
-//implementei um dicionário simples.
-typedef struct {
-    int key;
-    int value;
-} dict;
-dict actuatorsDict[NUM_ACTUATORS];
+int sensorsId[NUM_SENSORS];
+//Como 
+int actuatorsKey[NUM_ACTUATORS];
+int actuatorsValue[NUM_ACTUATORS] = {0};
+//Declaração de semáforos e mutexes
+sem_t sensorialDataBufferEmpty, sensorialDataBufferFull;
+pthread_mutex_t createSensorialDataMutex, modifyActuatorValueMutex;
 
-//função para adicionar um novo par chave-valor ao dicionário
-void addToDict(int key, int value, int index) {
-    actuatorsDict[index].key = key;
-    actuatorsDict[index].value = value;
-}
-//função que inicia os atuadores com chave-valor de -1, 0 (Iniciando os atuadores com valor 0)
-void startDict() {
-    for (int i = 0; i < NUM_ACTUATORS; i++) {
-        addToDict(-1, 0, i);
+
+
+//Função para criar um novo dado sensorial
+void *createSensorialData(void *arg) {
+    int thread_num = *((int *) arg);
+    while(TRUE) {
+        sem_wait(&sensorialDataBufferEmpty);
+        srand(time(NULL));
+        //Intervalo para conseguir um novo dado sensorial entre 1 a 5 segundos
+        sleep((rand() % 5) + 1);
+        printf("%d\n", (rand()%1000 + 1));
+        sem_post(&sensorialDataBufferFull);
     }
 }
 
-//Função para criar um novo dado sensorial
-void createSensorialData() {
-    sem_wait(&sensorialDataBufferEmpty);
-    //Intervalo para conseguir um novo dado sensorial entre 1 a 5 segundos
-    sleep((rand() % 5) + 1);
-    pthread_mutex_lock(&createSensorialDataMutex);
-    sensorialData append
-    pthread_mutex_unlock(&createSensorialDataMutex);
-    sem_post(&sensorialDataBufferFull);
-}
-
-void readSensorialData() {
+// void readSensorialData() {
     // pass;
-}
+// }
 
 void controlCenter() {
     sem_wait(&sensorialDataBufferFull);
@@ -54,32 +46,36 @@ void controlCenter() {
 }
 
 void modifyActuatorValue() {
-    sleep((rand() % 2) + 2)
+    sleep((rand() % 2) + 2);
 
     pthread_mutex_lock(&modifyActuatorValue);
     printf("Alterando: <ATUADOR> com valor <NIVEL DE ATIVIDADE>!\n");
-    sleep(1)
+    sleep(1);
     pthread_mutex_unlock(&modifyActuatorValue);
 
 }
 
-
 int main() {
-    //Declaração dos semáforos, não sei se tem como inicializar eles direto, como os mutexes.
-    sem_t createSensorialDataSemaphore, readSensorialDataSemaphore;
-    //Inicializando os mutexes
-    pthread_mutex_t createSensorialDataMutex = PTHREAD_MUTEX_INITIALIZER;
-    pthread_mutex_t modifyActuatorValueMutex = PTHREAD_MUTEX_INITIALIZER;
+    //Inicialização dos semáforos e mutexes
     sem_init(&sensorialDataBufferEmpty, 0, NUM_SENSORS); //Semáforo para produção
     sem_init(&sensorialDataBufferFull, 0, 0); //Semáforo para consumo
-    srand(time(NULL));
-    
-    startDict();
+    pthread_mutex_init(&createSensorialDataMutex, NULL);
+    pthread_mutex_init(&modifyActuatorValueMutex, NULL);
 
-    for(int i = 0; i < NUM_ACTUATORS; i++) {
-        printf("Key: %d, Value: %d\n", actuatorsDict[i].key, actuatorsDict[i].value);
+
+    for(int i = 0; i < NUM_SENSORS; i++) {
+        sensorsId[i] = i;
+        sleep(1);
+        pthread_create(&sensorsThread[i], NULL, createSensorialData, &sensorsId[i]);
     }
 
+    for(int i = 0; i < NUM_ACTUATORS; i++) {
+        printf("Key: %d\n", actuatorsValue[i]);
+    }
+
+    for(int i = 0; i < NUM_SENSORS; i++) {
+        pthread_join(sensorsThread[i], NULL);
+    }
     //Destruição dos semáforos e mutexes
     sem_destroy(&sensorialDataBufferEmpty);
     sem_destroy(&sensorialDataBufferFull);
